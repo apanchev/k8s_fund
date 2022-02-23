cd370a6a52f6d7bc9130b334fe403d480c4e61fb
apanchev <contact@apanchev.eu>
2022-02-23 18:38:56 +0100

    upadte provider version and fix script

--- linuxfondation.sh
+++ linuxfondation.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 PS3='
 Choose what to do: '
-choices=("Start the project" "Destroy current intances" "Launch Ansible")
+choices=("Start the project" "Destroy intances" "Launch Ansible config")
 select choice in "${choices[@]}"; do
   case $choice in
     "Start the project")
       cd terraform
+      terraform init
       terraform plan -var-file aws.tfvars
       terraform apply -var-file aws.tfvars
       cd ../ansible
@@ -21,12 +22,12 @@ select choice in "${choices[@]}"; do
       ansible-playbook -i ansible_hosts ansible_playbook_config.yml
       exit
       ;;
-    "Destroy current intances")
+    "Destroy intances")
       cd terraform
       terraform apply -destroy -var-file aws.tfvars
       exit
       ;;
-    "Launch Ansible")
+    "Launch Ansible config")
       cd ansible
       ansible-playbook -i ansible_hosts ansible_playbook_config.yml
       exit
